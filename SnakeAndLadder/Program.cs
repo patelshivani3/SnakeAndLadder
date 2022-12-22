ca48aa3ec9ea996de99818c4eb71660fbc4c391d
patelshivani3 <shivpatel3001@gmail.com>
2022-12-22 16:33:41 +0530

    Adding Option For No Play

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,11 +11,36 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Snake and Ladder Program");
-
-            int position = 0;
+            //UC1 initialize the position of the player
+            int position = 0 , die =0;
+            
+            //UC2 Random roller die number 
             Random random= new Random();
-            int die = random.Next(1, 7);
-            Console.WriteLine("The value on rolling die : "+die);
+            die = random.Next(1, 7);
+            
+            // UC3 Check No Play ,Ladder and Snake Option
+            int check = random.Next(0, 3);
+            if (check == 1)
+            {
+                Console.WriteLine("Ladder");
+                position = position + die;
+                Console.WriteLine("Die Number is {0} Player is at position {1}  ", die, position);
+            }
+            else if (check == 2)
+            {
+                Console.WriteLine("Snake");
+                position = position - die;
+                if (position < 0)
+                    position = 0;
+                Console.WriteLine("Die Number is {0} Player is at position {1}  ", die, position);
+            }
+            else
+            {
+                Console.WriteLine("No Play");
+                Console.WriteLine("Player is at position : "+position);
+            }
+                
+
             Console.ReadLine();
         }
     }
