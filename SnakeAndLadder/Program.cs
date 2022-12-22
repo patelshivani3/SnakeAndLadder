1a097490b541c0456d25eb7309404dc79f754ac5
patelshivani3 <shivpatel3001@gmail.com>
2022-12-22 17:30:31 +0530

    Adding last position for player

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -11,49 +11,47 @@ namespace SnakeAndLadder
         static void Main(string[] args)
         {
             Console.WriteLine("\nWelcome to Snake and Ladder Program");
-<<<<<<< HEAD
+
             //UC1 initialize the position of the player
             int position = 0 , die =0;
             
             //UC2 Random roller die number 
             Random random= new Random();
-            die = random.Next(1, 7);
-            
-            // UC3 Check No Play ,Ladder and Snake Option
-            int check = random.Next(0, 3);
-            if (check == 1)
-            {
-                Console.WriteLine("Ladder");
-                position = position + die;
-                Console.WriteLine("Die Number is {0} Player is at position {1}  ", die, position);
-            }
-            else if (check == 2)
-            {
-                Console.WriteLine("Snake");
-                position = position - die;
-                if (position < 0)
-                    position = 0;
-                Console.WriteLine("Die Number is {0} Player is at position {1}  ", die, position);
-            }
-            else
-            {
-                Console.WriteLine("No Play");
-                Console.WriteLine("Player is at position : "+position);
-            }
-                
+            //die = random.Next(1, 7);
 
-=======
-           
-            //UC1 initialize the position of the player
-            int position = 0;
-            
-            //UC2 Random roller die number 
-            Random random = new Random();
-            int die = random.Next(1, 7);
-            position = position + die;
-            Console.WriteLine("The value on rolling die is : "+die);
-            Console.WriteLine("Player Position is : "+position);
->>>>>>> RollTheDieUC2
+            // UC4 Check until the winning psition 100
+            while (position < 100)
+            {
+                die = random.Next(1, 7);
+
+                // UC3 Check No Play ,Ladder and Snake Option
+                int check = random.Next(0, 3);
+                if (check == 1)
+                {
+                    Console.WriteLine("\nLadder");
+                    position = position + die;
+                   // if (position > 100)
+                   // {
+                   //     position = 100;
+                   // }
+                    Console.WriteLine("Die:{0}   Position:{1}  ", die, position);
+                }
+                else if (check == 2)
+                {
+                    Console.WriteLine("\nSnake");
+                    position = position - die;
+                    if (position < 0)
+                    {
+                        position = 0;
+                    }
+                    Console.WriteLine("Die:{0}   Position:{1}  ", die, position);
+                }
+                else
+                {
+                    Console.WriteLine("\nNo Play");
+                    Console.WriteLine("Player is at position : " + position);
+                }
+            }
             Console.ReadLine();
         }
     }
